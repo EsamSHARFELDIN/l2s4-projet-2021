1531622f7ca16fabdbeada05cb51aa9b615a6f88
Moncheaux Kilian <kilian.moncheaux.etu@niv-lille.fr>
2021-03-02 20:17:35 +0100

    Worker: implements methods

---
+++ src/game/Worker.java
@@ -0,0 +1,46 @@
+package game;
+
+/**
+ * this class models a worker. A worker is a type of unit used in the agricultural ("agricol") game. 
+ * Simply, the worker .... works.
+ * this class extends the Unit class (@see Unit).
+ * for further details @see "the original subjest of the project, section 2."
+ *
+ */
+public class Worker extends Unit {
+	/**
+	 * the gold quantity a worker initially have just after creation
+	 */
+	public static final int INITIAL_GOLD_QUANTITY = 0;
+	/**
+	 * create a worker with given place and player. initially, a worker has 0 gold coin.
+	 * 
+	 * @param place place where this worker will be put on. can be null iff this army is placed nowhere. 
+	 * @param player the player who control this army. Can be null iff this worker is not controlled by a player
+	 */
+	public Worker(Tile place, Player player) {
+		super(place, player);
+		this.goldQuantity = INITIAL_GOLD_QUANTITY;
+	}
+	/**
+	 * gives the gold coin given by this worker when the player choose to do nothing (in the 1st step on the turn).
+	 * It depends on this worker's place.
+	 * @see "Original subjects section 3.2, régles complémentaires en fonction des territoires."
+	 * @return the gold coin given by this worker depending on this worker's place, when the player choose to do nothng.
+	 */
+	public int goldcoinWhenplayerDoesNothing() { //TODO
+		//needs a method in Tile that gives some point when the player do nothing
+		return -1;		
+	}
+	/**
+	 * gives the value of the remuneration (salary) this worker need to be kept after one turn.
+	 * @return the remuneration (salary) this worker need to be kept after one turn, it depends on the type of place it is 
+	 * set on (mountain, plain, forest, ...)
+	 * @see "Original subjects section 3.2, régles complémentaires en fonction des territoires."
+	 */
+	@Override
+	public int cost() { //TODO
+		//needs a method in Tile that gives the salary value
+		return -1;
+	}	
+}
