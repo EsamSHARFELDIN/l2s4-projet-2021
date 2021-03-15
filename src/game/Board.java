c2af5858120b9f206a2dd4ac9dda8031c61cee90
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-03-15 23:19:58 +0100

    Board.java: add interface and documentation
    * Added documentation for the class Board.java

---
+++ src/game/Board.java
@@ -0,0 +1,79 @@
+package game;
+
+/**
+ * A class to model the board in different games. A board is composed of tiles
+ * of different types and is defined by a width and a height. A board is created
+ * following two rules: at least 2 in 3 tiles are of type ocean, and all tile
+ * that is not of type ocean must have a neighbouring tile which isn't either
+ * @version 15/03/21
+ */
+public class Board {
+    /**
+     * Construct a board defined by its width (horizontal size) and a height
+     * (vertical size), representing numbers of tiles
+     * @param width Horizontal size in number of tiles
+     * @param height Vertical size in number of tiles
+     * @throws IllegalArgumentException iff either <code>width</code> or
+     * <code>height</code> is negative
+     */
+    public Board(int width, int height) throws IllegalArgumentException;
+
+    /**
+     * Check whether the board is full, ie whether all of the occupable tiles
+     * are currently occupied by units
+     * @return <code>true</code> if all tiles are occupied, else
+     * <code>false</code>*/
+    public boolean isFull();
+
+    /**
+     * Return the tile located at the position of the board defined by the
+     * coordinates passed as parameters
+     * @param x Horizontal coordinate, should be an integer between
+     * <code>0</code> and <code>width - 1</code>
+     * @param y Vertical coordinate, should be an integer between
+     * <code>0</code> and <code>height - 1</code>
+     * @return The tile at the specified location
+     * @throws Exception iff either <code>x</code> or <code>y</code> is out
+     * of the specified bounds
+     */
+    public Tile tileAt(int x, int y) throws Exception;
+
+    /**
+     * Set the unit located at the position of the board defined by the
+     * coordinates passed as parameters
+     * @param x Horizontal coordinate, should be an integer between
+     * <code>0</code> and <code>width - 1</code>
+     * @param y Vertical coordinate, should be an integer between
+     * <code>0</code> and <code>height - 1</code>
+     * @param unit Unit to position on the tile specified by the coordinates
+     * @throws Exception iff either <code>x</code> or <code>y</code> is out
+     * of the specified bounds
+     */
+    public void setUnitAt(int x, int y, Unit unit) throws Exception;
+
+    /**
+     * Return the neighbouring tiles of the tile which occupies the specified
+     * coordinates on the board as an array of tiles. The neighbouring tiles
+     * occupy the position at the north, west, south and east of the specified
+     * tile
+     * @param x Horizontal coordinate, should be an integer between
+     * <code>0</code> and <code>width - 1</code>
+     * @param y Vertical coordinate, should be an integer between
+     * <code>0</code> and <code>height - 1</code>
+     * @return An array of tiles representing the neighbouring tiles. The array
+     * may contain 2, 3 or 4 elements depending on the position of the initial
+     * tile
+     * @throws Exception iff either <code>x</code> or <code>y</code> is out
+     * of the specified bounds
+     */
+    public Tile[] adjacentTiles(int x, int y) throws Exception;
+
+    /** Two-dimensional array of tiles, used to represent the game world */
+    protected Tile[][] tiles;
+
+    /** Width of the board, as a number of tiles */
+    protected int width;
+
+    /** Height of the board, as a number of tile */
+    protected int height;
+}
