988cd23a1fa5679925b0ae7f83ca054f10784563
zo tahina <zotahina.ratefiarivony.etu@univ-lille.fr>
2021-03-18 15:27:11 +0100

    class Action: javadoc ffor this class

--- src/game/Action.java
+++ src/game/Action.java
@@ -1,8 +1,20 @@
 package game;
 
+/**
+ * This class represents an action.
+ * More precisely,  it is an action among those the player 
+ * can choose each first part of the game.
+ * For example, in the War game, a player, in the first part of his turn,
+ * can choose between deploying and doing nothing.
+ * An action can be executed (realized).
+ *
+ */
 public interface Action {
-    
-	
-	
-	
+	/**
+	 * Realize this action with the given board and player.
+	 * 
+	 * @param board the board with which the action will be realized.
+	 * @param player the player that is realizing this action.
+	 */
+    void execute(Board board, Player player);
 }
