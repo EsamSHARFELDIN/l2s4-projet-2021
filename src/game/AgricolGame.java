e608ef74068b1f87e1f2fa8d43e545a269878e34
Moncheaux Kilian <kilian.moncheaux.etu@niv-lille.fr>
2021-04-01 15:32:17 +0200

    update src : Tile, Resource, Game and daughters

---
+++ src/game/AgricolGame.java
@@ -0,0 +1,34 @@
+package game;
+import game.*;
+
+/**
+  * A class which allows to model the war game.
+  */
+
+public class AgricolGame extends Game {
+
+    /**
+      * Creates an agricol game with a given board and given players.
+      * @param board a given board.
+      * @param players given players.
+      */
+    public AgricolGame(int board, int players){
+
+    }
+
+    /**
+      * initializes a new agricol game.
+      */
+    private void initialize(){
+
+    }
+
+    /**
+     * Start the agricol game and give the course of the game.
+     * @return Player the winner of the game.
+     */
+    @Override
+    public Player play() {
+        
+    }
+}
--- src/game/Tile.java
+++ src/game/Tile.java
@@ -1,4 +1,42 @@
 package game;
-public class Tile {
+import game.Resouce;
+import game.Unit;
+
+public abstract class Tile {
+    protected Unit unit;
+    /**
+     * 
+     */
+    public Tile(){
+
+    }
+    /**
+     * 
+     * @param u
+     * @return
+     */
+    public boolean isBusy(Unit u){
+        return true;
+    }
+    /**
+     * 
+     * @return
+     */
+    public abstract Resource getResource(){
+        return resource;
+    }   
+    /**
+     * 
+     * @return
+     */
+    public Unit getUnit(){
+
+    }
+    /**
+     * 
+     * @param u
+     */
+    public void setUnit(Unit u){
     
+    }
 }
