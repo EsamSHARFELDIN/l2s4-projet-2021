50b16ff232134a24a6d2fce1ab5d7184232d2f0c
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-04-01 13:15:35 +0200

    doc: document DoNothingAction
    * Added documentation for DoNothingAction.java

---
+++ src/game/DoNothingAction.java
@@ -0,0 +1,26 @@
+package game;
+
+/**
+ * A class which represents the action of not doing anything for a turn in a
+ * board game. This class corresponds to a waiting turn, an can be extended in
+ * cases where a wait has specific consequences.
+ */
+public class DoNothingAction implements Action {
+    /**
+     * Construct an action corresponding to a waiting turn, with no consequences
+     */
+    public DoNothingAction() {
+        // TODO
+    }
+
+    /**
+     * @see Action#execute(Board, Player)
+     * Execute a waiting action. Nothing happens, and arguments are only needed
+     * for consistency reasons
+     * @param board Playing board
+     * @param player Player taking the action
+     **/
+    public void execute(Board board, Player player) {
+        // TODO
+    }
+}
