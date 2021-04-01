1a3cea5d77fc15d70ab8abbf97841f157d7d33e4
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-04-01 13:14:39 +0200

    doc: document WarDeployAction
    * Added javadoc for WarDeployAction.java

---
+++ src/game/WarDeployAction.java
@@ -0,0 +1,31 @@
+package game;
+
+/**
+ * A class which represents the specific consequences of deploying a unit on
+ * a game tile in the war game. The deployment has military consequences on the
+ * surrounding tiles and on the occupying units
+ */
+public class WarDeployAction extends DeployAction {
+    /**
+     * Construct a deployment action specific to the war game, for a specified
+     * tile and a specified unit
+     * @param x Horizontal coordinate of the tile
+     * @param y Vertical coordinate of the tile
+     * @param u Deployed unit
+     */
+    public WarDeployAction(int x, int y, Unit u) {
+        // TODO
+    }
+
+    /**
+     * @see DeployAction#execute(Board, Player)
+     * Execute the action: deploy the unit and proceed with the appropriate
+     * military consequences on the surrounding tiles and units. The details of
+     * the rules can be found in the specification document
+     * @param board Playing board
+     * @param player Player who is deploying the unit
+     */
+    public void execute(Board board, Player player) {
+        // TODO
+    }
+}
