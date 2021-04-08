b0390ecd3b5082052a6192a7cbb61906c7537787
Moncheaux Kilian <kilian.moncheaux.etu@niv-lille.fr>
2021-04-08 14:33:22 +0200

    add : javadoc Tile.java

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -2,39 +2,44 @@ package game;
 import game.Resouce;
 import game.Unit;
 
+/**
+ * This class is use to represents the Tile of the board.
+ */
+
 public abstract class Tile {
     protected Unit unit;
     /**
-     * 
+     * Creates a new tile.
      */
     public Tile(){
 
     }
+
     /**
-     * 
-     * @param u
-     * @return
+     * This allows to know if a tile is busy or not.
+     * @return true if the tile is busy, otherwise false.
      */
-    public boolean isBusy(Unit u){
+    public boolean isBusy(){
         return true;
     }
+
     /**
-     * 
-     * @return
+     * Gives the ressource given by the tile.
      */
     public abstract Resource getResource(){
-        return resource;
-    }   
-    /**
-     * 
-     * @return
-     */
-    public Unit getUnit(){
 
     }
+
     /**
-     * 
-     * @param u
+     * Gives the unit posted on the tile.
+     * @return The unit posted on the tile.
+     */
+    public Unit getUnit(){
+        return unit;
+    }
+    /**
+     * This allows to change the unit post on the tile.
+     * @param u the unit that we want to post on the tile
      */
     public void setUnit(Unit u){
     
