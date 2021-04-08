3b1922007a1e0825aa46c220c5673e54ad444c63
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-04-08 14:17:57 +0200

    doc: correction constructor arguments
    * Minor correction in documentation of Game constructor

--- src/game/Game.java
+++ src/game/Game.java
@@ -3,27 +3,28 @@ import game.Board;
 import game.Player;
 
 /**
-  * An abstract class which allows to model the game.
-  */
+ * An abstract class which allows to model the game.
+ */
 
 public abstract class Game {
     protected Board board;
     protected List<Player> players;
-    
+
     /**
-     * Creates a game with a given board and given players.
-     * @param board a given board.
-     * @param players given players.
+     * Creates a game with a board of given dimensions and an empty list
+     * of players
+     * @param width Width of the board to use
+     * @param height Height of the board to use
      */
-    public Game(int board, int players) {
-        
+    public Game(int width, int height) {
+
     }
 
     /**
      * adds a player.
      * @param p The player to add.
      */
-    public void addPlayer(Player p){
+    public void addPlayer(Player p) {
 
     }
 
@@ -31,7 +32,7 @@ public abstract class Game {
      * removes a player
      * @param p The player to remove.
      */
-    public void removePlayer(Player p){
+    public void removePlayer(Player p) {
 
     }
 
@@ -39,15 +40,15 @@ public abstract class Game {
      * Start the game and give the course of the game.
      * @return Player the winner of the game.
      */
-    public abstract Player play(){
-        
+    public abstract Player play() {
+
     }
-    
+
     /**
      * Makes sure the game is over.
      * @return True if the game is over, False otherwise.
      */
-    private Boolean isGameOver(){
+    private Boolean isGameOver() {
 
     }
 }
