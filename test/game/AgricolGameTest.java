318c8f29262946140d2caacef2932ba51a584a98
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-04-13 09:32:19 +0200

    test: add WarGameTest and AgricolGameTest
    * Added classes to test WarGame and AgricolTest (can only test
    constructor and play method)

---
+++ test/game/AgricolGameTest.java
@@ -0,0 +1,32 @@
+package game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AgricolGameTest {
+    @Test
+    public void canConstructGame() {
+        Game g = new Agricolgame(20, 12);
+        Player p1 = new AgricolPlayer("p1");
+        Player p2 = new AgricolPlayer("p2");
+        Player p3 = new AgricolPlayer("p3");
+        g.addPlayer(p1);
+        g.addPlayer(p2);
+        g.addPlayer(p3);
+    }
+
+    @Test
+    public void canPlay() {
+        Game g = new Agricolgame(20, 12);
+        Player p1 = new AgricolPlayer("p1");
+        Player p2 = new AgricolPlayer("p2");
+        Player p3 = new AgricolPlayer("p3");
+        g.addPlayer(p1);
+        g.addPlayer(p2);
+        g.addPlayer(p3);
+        Player winner = g.play();
+        assertTrue(winner == p1 || winner == p2 || winner == p3);
+    }
+}
