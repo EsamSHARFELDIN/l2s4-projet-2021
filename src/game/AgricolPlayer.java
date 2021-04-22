057bbd5c4b64495d5deed22c553ebc33ceb14bcc
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-04-22 15:12:02 +0200

    doc: chooseAction, remunerate, canRemunerate abstract
     * Methods chooseAction, remunerate and canRemunerate were modified
       to be abstract in class Player
     * Added prototype and doc for these methods in WarPlayer and AgricolPlayer

--- src/game/AgricolPlayer.java
+++ src/game/AgricolPlayer.java
@@ -4,36 +4,48 @@ package game;
  * This class modelize a Player of an agricol game.
  * This class extends the Unit class (@see Player).
  */
+public class AgricolPlayer extends Player {
 
-public class AgricolPlayer extends Player{
-	
-	/**
-    * create a Player with given name.
-    * @param name the name of the Player.
-    */
-	public AgricolPlayer(String name, int rockStock, int sandStock, int wheatStock, int woodStock, int goldStock) {
-		super(name, rockStock, sandStock, wheatStock, woodStock, goldStock);
-		
-	}
-	/**
-	 * create a player for the agricol game with the given name.
-	 * 
-	 * Initially, this player has 15 gold coin and no ressources.
-	 * @param name name of this player
-	 */
-	public AgricolPlayer(String name) {
-		super("Bar", 0, 0, 0, 0, 0);
-	}
-	
+    /**
+     * create a player for the agricol game with the given name.
+     *
+     * Initially, this player has 15 gold coin and no ressources.
+     * @param name name of this player
+     */
+    public AgricolPlayer(String name) {
+        super("Bar", 0, 0, 0, 0, 0);
+    }
 
-	/**
-    * convert the player's resource in gold.
-    */
-	@Override
-	public void convertResource() {
-		
-	}
+    /**
+     * Return an action among those specific to the agricultural game
+     * @return Action specific to the agricultural game
+     */
+    public Action chooseAction() {
 
-	
-    
+    }
+
+    /**
+     * Pay the maintenance cost of the unit
+     * @param unit Unit to feed
+     */
+    public void remunerate(Unit unit) {
+
+    }
+
+    /**
+     * Return <code>true</code> if the player has the resources to pay the
+     * maintenance cost of the unit, else <code>false</code>
+     * @return A boolean indicating if the player can feed the unit
+     */
+    public boolean canRemunerate() {
+
+    }
+
+    /**
+     * convert the player's resource in gold.
+     */
+    @Override
+    public void convertResource() {
+
+    }
 }
--- src/game/WarPlayer.java
+++ src/game/WarPlayer.java
@@ -4,95 +4,106 @@ package game;
  * This class modelize a Player of a war game.
  * This class extends the Unit class (@see Player).
  */
-
 public class WarPlayer extends Player {
-	/**
-	 * the player's stock of warrior (35 at the start).
-	 */
-	
-	protected int warriorStock = 35;
-	/**
-	 * the player's stock of food.
-	 */
-	protected int foodStock;
-	/**
-	 * create a player with given name
-	 * @param name the name of player
-	 * @param rockStock
-	 * @param sandStock
-	 * @param wheatStock
-	 * @param woodStock
-	 * @param goldStock
-	 * @param warriorStock
-	 * @param foodStock
-	 */
-	public WarPlayer(String name, int rockStock, int sandStock, int wheatStock, int woodStock, int goldStock, int warriorStock, int foodStock) {
-		super(name, rockStock, sandStock, wheatStock, woodStock, goldStock);
-		this.warriorStock = warriorStock;
-		this.foodStock = foodStock;
-	}
-	/**
-	 * Create a player for the war game with given name.
-	 * 
-	 * Initially the player has 35 warriors, 10 food unit and 0 gold coin.
-	 * @param name name of this player.
-	 */
-	public WarPlayer(String name) {
-		super("Foo", 0, 0, 0, 0, 0);
-	}
-	
-	/**
-	 * convert the player's resource in food.
-	 */
-	@Override
-	public void convertResource() {
-				
-	}
-	/**
-	 * gives the number of warriors remaining to the player.
+    /**
+     * the player's stock of warrior (35 at the start).
+     */
+    protected int warriorStock = 35;
+
+    /**
+     * the player's stock of food.
+     */
+    protected int foodStock;
+
+    /**
+     * Create a player for the war game with given name.
+     *
+     * Initially the player has 35 warriors, 10 food unit and 0 gold coin.
+     * @param name name of this player.
+     */
+    public WarPlayer(String name) {
+        super("Foo", 0, 0, 0, 0, 0);
+    }
+
+    /**
+     * Return an action among those specific to the war game
+     * @return Action specific to the war game
+     */
+    public Action chooseAction() {
+
+    }
+
+    /**
+     * Pay the maintenance cost of the unit
+     * @param unit Unit to feed
+     */
+    public void remunerate(Unit unit) {
+
+    }
+
+    /**
+     * Return <code>true</code> if the player has the resources to pay the
+     * maintenance cost of the unit, else <code>false</code>
+     * @return A boolean indicating if the player can feed the unit
+     */
+    public boolean canRemunerate() {
+
+    }
+
+    /**
+     * convert the player's resource in food.
+     */
+    @Override
+    public void convertResource() {
+
+    }
+
+    /**
+     * gives the number of warriors remaining to the player.
      * @return the number of warriors remaining to the player.
-	 */
-	public int getWarriorStock() {
-		return warriorStock;
-	}
-	/**
-	 * gives the stock of food of the player.
+     */
+    public int getWarriorStock() {
+        return warriorStock;
+    }
+
+    /**
+     * gives the stock of food of the player.
      * @return the player's stock of food.
-	 */
-	public int getFoodStock() {
-		return foodStock;
-	}
-	/**
+     */
+    public int getFoodStock() {
+        return foodStock;
+    }
+
+    /**
      * increase the player's stock of warrior.
      * @param incr the value of the increment of the Player's warrior stock.
      */
-	
-	public void incrementWarriorStock(int incr) {
-		
-	}
-	
-	/**
-	 * increase the player's stock of food.
+    public void incrementWarriorStock(int incr) {
+
+    }
+
+    /**
+     * increase the player's stock of food.
      * @param incr the value of the increment of the Player's food stock.
      */
-	public void incrementFoodStock(int incr) {
-		
-	}
-	/**
+    public void incrementFoodStock(int incr) {
+
+    }
+
+    /**
      * decrease the player's stock of warrior.
      * @param decr the value of the decrement of the Player's warrior stock.
      */
+    public void decrementWarriorStock(int decr) {
 
-	public void decrementWarriorStock(int decr) {
-		
-	}
-	/**
-	 * decrease the player's stock of food.
+    }
+
+    /**
+     * decrease the player's stock of food.
      * @param decr the value of the decrement of the Player's food stock.
 
-	 */
-	public void decrementFoodStock(int decr) {
-		
-	}
-	
+     */
+    public void decrementFoodStock(int decr) {
+
+    }
 }
