98945b40377a9e30e57885f0b3dcd7f634086593
Moncheaux Kilian <kilian.moncheaux.etu@niv-lille.fr>
2021-05-10 19:01:23 +0200

    add: Début implémentation WarGame.java

--- src/game/WarGame.java
+++ src/game/WarGame.java
@@ -5,20 +5,56 @@ import game.*;
  * A class which allows to model the war game.
  */
 public class WarGame extends Game {
+    
+    protected int turns_counter;
+
     /**
      * Creates a war game with a board of given dimensions
      * @param width Width of the board to use
      * @param height Height of the board to use
      */
     public WarGame(int width, int height){
-
+      super(width, height);
+      this.turns_counter = 0;
+      initialize();
     }
 
     /**
      * initializes a new war game.
      */
     private void initialize(){
+      Resource.setConversionValue(Resource.Stone, 0);
+      Resource.setConversionValue(Resource.Sand, 0);
+      Resource.setConversionValue(Resource.Wood, 1);
+      Resource.setConversionValue(Resource.Wheat, 5);
 
+      PlainTile.MAX_ARMY_SIZE = 5;
+      PlainTile.ADDITIONAL_POWER = 0;
+      PlainTile.ADDITIONAL_POINTS = 1;
+      PlainTile.GOLD_WHEN_DOING_NOTHING = 0;
+      PlainTile.COST_ADD = 0;
+      PlainTile.COST_FACTOR = 1;
+
+      MountainTile.MAX_ARMY_SIZE = 3;
+      MountainTile.ADDITIONAL_POWER = 2;
+      MountainTile.ADDITIONAL_POINTS = 4;
+      MountainTile.GOLD_WHEN_DOING_NOTHING = 0;
+      MountainainTile.COST_ADD = 0;
+      MountainTile.COST_FACTOR = 1;
+
+      ForestTile.MAX_ARMY_SIZE = 5;
+      ForestTile.ADDITIONAL_POWER = 0;
+      ForestTile.ADDITIONAL_POINTS = 2;
+      ForestTile.GOLD_WHEN_DOING_NOTHING = 0;
+      ForestTile.COST_ADD = 0;
+      ForestTile.COST_FACTOR = 1;
+
+      DesertTile.MAX_ARMY_SIZE = 3;
+      DesertTile.ADDITIONAL_POWER = 0;
+      DesertTile.ADDITIONAL_POINTS = 4;
+      DesertTile.GOLD_WHEN_DOING_NOTHING = 0;
+      DesertTile.COST_ADD = 0;
+      DesertTile.COST_FACTOR = 2;
     }
 
     /**
