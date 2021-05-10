f4fc675f9ba53d15df2de02649afb44b239b1461
Moncheaux Kilian <kilian.moncheaux.etu@niv-lille.fr>
2021-05-10 19:01:54 +0200

    add: Implémentation Ressource.java & Tile.java

--- src/game/Resource.java
+++ src/game/Resource.java
@@ -9,18 +9,31 @@ public enum Resource {
   
 	Stone, Sand, Wheat, Wood;
 	
-	static int STONE_CONVERSION_VALUE;
-	static int SAND_CONVERSION_VALUE;
-	static int WHEAT_CONVERSION_VALUE;
-	static int WOOD_CONVERSION_VALUE;
+	private static int STONE_CONVERSION_VALUE;
+	private static int SAND_CONVERSION_VALUE;
+	private static int WHEAT_CONVERSION_VALUE;
+	private static int WOOD_CONVERSION_VALUE;
 
   /*
   * set the conversion value of the resource r to n.
   * @param r the resource that we want to modify the conversion value.
   * @param i the new conversion value.
   */
-	public static void setConversionValue(Resource r, int i){
-
+	public static void setConversionValue(Resource r, int i) {
+    switch (r) {
+      case Stone:
+      STONE_CONVERSION_VALUE = i;
+      break;
+      case Wheat:
+      WHEAT_CONVERSION_VALUE = i;
+      break;
+      case Wood:
+      WOOD_CONVERSION_VALUE = i;
+      break;
+      case Sand : 
+      SAND_CONVERSION_VALUE = i;
+      break;
+    }
 	}
 
   /*
@@ -28,8 +41,18 @@ public enum Resource {
   * @param r the resource we want to get the conversion value.
   * @return the conversion value of the resource.
   */
-	public static int getConversionValue(Resource r){
-		return -1;
+	public static int getConversionValue(Resource r) {
+		switch (r) {
+      case Stone:
+      return STONE_CONVERSION_VALUE;
+      case Wheat:
+      return WHEAT_CONVERSION_VALUE;
+      case Wood:
+      return WOOD_CONVERSION_VALUE;
+      case Sand : 
+      return SAND_CONVERSION_VALUE;
+      default:
+      return -1; /* ne devrait pas arriver */
+    }
 	}
-
 }
--- src/game/Tile.java
+++ src/game/Tile.java
@@ -1,147 +1,148 @@
 package game;
+
 import game.Resource;
 import game.Unit;
 
 /**
  * This class is use to represents the Tile of the board.
  */
-
 public abstract class Tile {
-    /** Unit occupying the tile */
-    protected Unit unit;
+  /** Unit occupying the tile */
+  protected Unit unit;
 
-    /** Horizontal coordinate of the tile */
-    protected int x;
+  /** Horizontal coordinate of the tile */
+  protected int x;
 
-    /** Vertical coordinate of the tile */
-    protected int y;
+  /** Vertical coordinate of the tile */
+  protected int y;
 
-    /**
-     * Creates a new tile.
-     */
-    public Tile(int x, int y) {
-        this.unit = null;
-        this.x = x;
-        this.y = y;
+  /**
+   * Creates a new tile.
+   */
+  public Tile(int x, int y) {
+    this.unit = null;
+    this.x = x;
+    this.y = y;
+  }
+
+  /**
+   * Return the horizontal coordinate of the tile
+   * 
+   * @return Horizontal coordinate
+   */
+  public int getX() {
+    return this.x;
+  }
+
+  /**
+   * Return the vertical coordinate of the tile
+   * 
+   * @return Vertical coordinate
+   */
+  public int getY() {
+    return this.y;
+  }
+
+  /**
+   * This allows to know if a tile is busy or not.
+   * 
+   * @return true if the tile is busy, otherwise false.
+   */
+  public boolean isBusy() {
+    return (this.unit != null);
+  }
+
+  /**
+   * Gives the ressource given by the tile.
+   */
+  public abstract Resource getResource() throws IllegalGameActionException;
+
+  /**
+   * Gives the unit posted on the tile.
+   * 
+   * @return The unit posted on the tile.
+   */
+  public Unit getUnit() {
+    return this.unit;
+  }
+
+  /**
+   * This allows to change the unit post on the tile.
+   * 
+   * @param u the unit that we want to post on the tile
+   * @throws IllegalArgumentException when null is given as an argument.
+   * @throws IllegalGameActionException if the set is forbidden (eg. on OceanTile)
+   */
+  public void setUnit(Unit u) throws IllegalArgumentException, IllegalGameActionException {
+    if (u == null) {
+      throw new IllegalArgumentException("Tried to set null as unit");
     }
+    this.unit = u;
+  }
 
-    /**
-     * Return the horizontal coordinate of the tile
-     * @return Horizontal coordinate
-     */
-    public int getX() {
-        return this.x;
-    }
+  /**
+   * Make sure that there is no unit placed in this tile after the call of this
+   * method.
+   */
+  public void unsetUnit() {
+    this.unit = null;
+  }
 
-    /**
-     * Return the vertical coordinate of the tile
-     * @return Vertical coordinate
-     */
-    public int getY() {
-        return this.y;
-    }
+  /**
+   * return the bonus gold a player may receive from each of his unit placed on
+   * this type of tile when among the selectable actions he chooses to do nothing.
+   * 
+   * <code>GOLD_WHEN_DOING_NOTHING</code> has to be initialized before running the
+   * game (Game#play) to avoid undefined behavior.
+   * 
+   * @return The bonus gold a player may receive from each of his unit placed on
+   *         this type of tile when among the selectable actions he chooses to do
+   *         nothing.
+   */
+  public abstract int getGoldWhenDoingNothing();
 
-    /**
-     * This allows to know if a tile is busy or not.
-     * @return true if the tile is busy, otherwise false.
-     */
-    public boolean isBusy() {
-        return true;
-    }
+  /**
+   * return The amounts of additional (bonus) power that an unit placed on this
+   * type of tile may receives.
+   * 
+   * @return The amounts of additional (bonus) power that an unit placed on this
+   *         type of tile may receives.
+   */
+  public abstract int getAdditionnalPower();
 
-    /**
-     * Gives the ressource given by the tile.
-     */
-    public abstract Resource getResource() throws IllegalGameActionException;
+  /**
+   * return the bonus a player may receive from each of his unit placed on this
+   * type of tile when counting the total points at the end of the game.
+   * 
+   * @return The bonus a player may receive from each of his unit placed on this
+   *         type of tile when counting the total points at the end of the game.
+   */
+  public abstract int getAdditionnalPoints();
 
-    /**
-     * Gives the unit posted on the tile.
-     * @return The unit posted on the tile.
-     */
-    public Unit getUnit() {
-        return unit;
-    }
+  /**
+   * Return the maximum army size allowed on a tile
+   * 
+   * @return Maximum army size
+   */
+  public abstract int getMaxArmySize();
 
-    /**
-     * This allows to change the unit post on the tile.
-     * @param u the unit that we want to post on the tile
-     * @throws IllegalArgumentException when null is given as
-     * an argument.
-     */
-    public void setUnit(Unit u) throws IllegalArgumentException, IllegalGameActionException {
+  public abstract void print();
 
-    }
+  /**
+   * return the bonus to be added to the initial cost of an unit placed on this
+   * tile.
+   * 
+   * @return the bonus to be added to the initial cost of an unit placed on this
+   *         tile.
+   */
+  public abstract int getCostAdd();
 
-    /**
-     * Make sure that there is no unit placed in this tile
-     * after the call of this method.
-     */
-    public void unsetUnit() {
-        //TODO
-    }
-    
-    /**
-     * return the bonus gold a player may receive from each of
-     * his unit placed on this type of tile when among
-     * the selectable actions he chooses to do nothing.
-     * 
-     * <code>GOLD_WHEN_DOING_NOTHING</code> has to be 
-     * initialized before running the game (Game#play) 
-     * to avoid undefined behavior.
-     * 
-     * @return The bonus gold a player may receive from each of
-     * his unit placed on this type of tile when among
-     * the selectable actions he chooses to do nothing.
-     */
-    public abstract int getGoldWhenDoingNothing();
-    
-    /**
-     * return The amounts of additional (bonus) power
-     * that an unit placed on this type of tile
-     * may receives.
-     * 
-     * @return The amounts of additional (bonus) power
-     * that an unit placed on this type of tile
-     * may receives.
-     */
-    public abstract int getAdditionnalPower();
-    
-    /** 
-     * return the bonus a player may receive from each of
-     * his unit placed on this type of tile when
-     * counting the total points at the end of the
-     * game.
-     * 
-     * @return The bonus a player may receive from each of
-     * his unit placed on this type of tile when
-     * counting the total points at the end of the
-     * game.
-     */
-    public abstract int getAdditionnalPoints();
-
-    /**
-     * Return the maximum army size allowed on a tile
-     * @return Maximum army size
-     */
-    public abstract int getMaxArmySize();
-
-    public abstract void print();
-    
-    /**
-     * return the bonus to be added to the initial cost 
-     * of an unit placed on this tile.
-     * 
-     * @return the bonus to be added to the initial cost
-     * of an unit placed on this tile.
-     */
-    public abstract int getCostAdd();
-    
-    /**
-     * return the factor with which to multiply the
-     * initial cost of an unit placed on this tile.
-     * 
-     * @return the factor with which to multiply the
-     * initial cost of an unit placed on this tile.
-     */
-    public abstract int getCostFactor();
+  /**
+   * return the factor with which to multiply the initial cost of an unit placed
+   * on this tile.
+   * 
+   * @return the factor with which to multiply the initial cost of an unit placed
+   *         on this tile.
+   */
+  public abstract int getCostFactor();
 }
