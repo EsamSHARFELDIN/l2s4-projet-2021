a6de3c48ed85ec109d76bb305ddb93a4d76aa695
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-05-10 23:55:46 +0200

    impl: refactor AgricolGame::play
     * Added private method findWinner
     * Minor refactor in AgricolGame

--- src/game/AgricolGame.java
+++ src/game/AgricolGame.java
@@ -18,56 +18,64 @@ public class AgricolGame extends Game {
      * @param height Height of the board to use
      */
     public AgricolGame(int width, int height) {
-      super(width, height);
-      this.turns_counter = 0;
-      initialize();
+        super(width, height);
+        this.turns_counter = 0;
     }
 
     /**
      * initializes a new agricol game.
      */
     private void initialize() {
-      Resource.setConversionValue(Resource.Stone, 8);
-      Resource.setConversionValue(Resource.Sand, 5);
-      Resource.setConversionValue(Resource.Wood, 2);
-      Resource.setConversionValue(Resource.Wheat, 2);
+        Resource.setConversionValue(Resource.Stone, 8);
+        Resource.setConversionValue(Resource.Sand, 5);
+        Resource.setConversionValue(Resource.Wood, 2);
+        Resource.setConversionValue(Resource.Wheat, 2);
 
-      PlainTile.MAX_ARMY_SIZE = 1;
-      PlainTile.ADDITIONAL_POWER = 0;
-      PlainTile.ADDITIONAL_POINTS = 0;
-      PlainTile.GOLD_WHEN_DOING_NOTHING = 1;
-      PlainTile.COST_ADD = 1;
-      PlainTile.COST_FACTOR = 1;
+        PlainTile.MAX_ARMY_SIZE = 1;
+        PlainTile.ADDITIONAL_POWER = 0;
+        PlainTile.ADDITIONAL_POINTS = 0;
+        PlainTile.GOLD_WHEN_DOING_NOTHING = 1;
+        PlainTile.COST_ADD = 1;
+        PlainTile.COST_FACTOR = 1;
 
-      MountainTile.MAX_ARMY_SIZE = 1;
-      MountainTile.ADDITIONAL_POWER = 0;
-      MountainTile.ADDITIONAL_POINTS = 0;
-      MountainTile.GOLD_WHEN_DOING_NOTHING = 0;
-      MountainTile.COST_ADD = 5;
-      MountainTile.COST_FACTOR = 1;
+        MountainTile.MAX_ARMY_SIZE = 1;
+        MountainTile.ADDITIONAL_POWER = 0;
+        MountainTile.ADDITIONAL_POINTS = 0;
+        MountainTile.GOLD_WHEN_DOING_NOTHING = 0;
+        MountainTile.COST_ADD = 5;
+        MountainTile.COST_FACTOR = 1;
 
-      ForestTile.MAX_ARMY_SIZE = 1;
-      ForestTile.ADDITIONAL_POWER = 0;
-      ForestTile.ADDITIONAL_POINTS = 0;
-      ForestTile.GOLD_WHEN_DOING_NOTHING = 1;
-      ForestTile.COST_ADD = 1;
-      ForestTile.COST_FACTOR = 1;
+        ForestTile.MAX_ARMY_SIZE = 1;
+        ForestTile.ADDITIONAL_POWER = 0;
+        ForestTile.ADDITIONAL_POINTS = 0;
+        ForestTile.GOLD_WHEN_DOING_NOTHING = 1;
+        ForestTile.COST_ADD = 1;
+        ForestTile.COST_FACTOR = 1;
 
-      DesertTile.MAX_ARMY_SIZE = 1;
-      DesertTile.ADDITIONAL_POWER = 0;
-      DesertTile.ADDITIONAL_POINTS = 0;
-      DesertTile.GOLD_WHEN_DOING_NOTHING = 2;
-      DesertTile.COST_ADD = 3;
-      DesertTile.COST_FACTOR = 1;
+        DesertTile.MAX_ARMY_SIZE = 1;
+        DesertTile.ADDITIONAL_POWER = 0;
+        DesertTile.ADDITIONAL_POINTS = 0;
+        DesertTile.GOLD_WHEN_DOING_NOTHING = 2;
+        DesertTile.COST_ADD = 3;
+        DesertTile.COST_FACTOR = 1;
+    }
 
-      /*
-      MAX_ARMY_SIZE;
-      ADDITIONAL_POWER
-      ADDITIONAL_POINTS
-      GOLD_WHEN_DOING_NOTHING
-      COST_ADD
-      COST_FACTOR
-      */
+    /**
+     * Return the player with the highest score in the list. It is assumed that
+     * there is at least one player in the game
+     * @return Winning player
+     */
+    private Player findWinner() {
+        Player winner = this.players.get(0);
+        int maxScore = winner.score();
+        for (Player player : this.players) {
+            int currentScore = player.score();
+            if (currentScore > maxScore) {
+                winner = player;
+                maxScore = currentScore;
+            }
+        }
+        return winner;
     }
 
     /**
@@ -76,40 +84,34 @@ public class AgricolGame extends Game {
      */
     @Override
     public Player play() throws GameException {
-      if (this.players.isEmpty()) {
-        throw new RuntimeException("Tried to play with no players");
-      }
-      /* Initialiser itérateur au début de la liste des joueurs */
-      Iterator<Player> it = this.players.listIterator();
+        initialize();
+        if (this.players.isEmpty()) {
+            throw new RuntimeException("Tried to play with no players");
+        }
+        /* Initialiser itérateur au début de la liste des joueurs */
+        Iterator<Player> it = this.players.listIterator();
 
-      Player p = null;
-      while (!this.isGameOver()) {
-        if (it.hasNext()) {
-          p = it.next();
+        while (!this.isGameOver()) {
+            Player p;
+            if (it.hasNext()) {
+                p = it.next();
+            }
+            else {
+                it = this.players.listIterator();
+                p = it.next();
+                this.turns_counter++;
+            }
+            /* choisir et réaliser une action */
+            Action action = p.chooseAction(this.board);
+            action.execute(this.board, p);
+            /* rémunérer les unités */
+            p.remunerateAll();
         }
-        else {
-          it = this.players.listIterator();
-          p = it.next();
-          this.turns_counter++;
-        }
-        /* réaliser une action */
-        Action action = p.chooseAction(this.board);
-        action.execute(this.board, p);
-        /* rémunérer les unités */
-        p.remunerateAll();
-      }
 
-      /* trouver le joueur au score maximal et le renvoyer */
-      p = this.players.get(0);
-      for (Player player : this.players) {
-        if (player.score() > p.score()) {
-          p = player;
-        }
-      }
-      return p;
+        return findWinner();
     }
 
     protected boolean isGameOver() {
-      return (this.board.isFull() || this.turns_counter == MAX_TURNS);
+        return (this.board.isFull() || this.turns_counter == MAX_TURNS);
     }
 }
