88b90f9017f356eb9618835a4a2eb859f61633e2
zo <zotahina.ratefiarivony.etu@univ-lille.fr>
2021-05-15 19:11:37 +0200

    test: WarDeployActionTest, fix canConstruct()

--- test/game/action/WarDeployActionTest.java
+++ test/game/action/WarDeployActionTest.java
@@ -7,17 +7,14 @@ import org.junit.Test;
 import game.game.Game;
 import game.player.Player;
 import game.player.WarPlayer;
+import game.unit.Unit;
+import game.unit.Worker;
 
-public class WarGameTest {
+public class WarDeployActionTest {
     @Test
-    public void canConstructGame() {
-        Game g = new Wargame(20, 12);
-        Player p1 = new WarPlayer("p1");
-        Player p2 = new WarPlayer("p2");
-        Player p3 = new WarPlayer("p3");
-        g.addPlayer(p1);
-        g.addPlayer(p2);
-        g.addPlayer(p3);
+    public void canConstruct() {
+    	Unit u = new Worker();
+        new WarDeployAction(0, 0, u);
     }
 
     @Test
