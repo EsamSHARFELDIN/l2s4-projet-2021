31382e6eedf355f82e0063792ded2d4b18422142
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-05-15 17:21:16 +0200

    test: fixed minor inconsistencies in TileMockTest
     * Fixed test after interface changes
     * Formated Tile

--- src/game/tile/Tile.java
+++ src/game/tile/Tile.java
@@ -8,142 +8,142 @@ import game.unit.Unit;
  * This class is use to represents the Tile of the board.
  */
 public abstract class Tile {
-  /** Unit occupying the tile */
-  protected Unit unit;
+    /** Unit occupying the tile */
+    protected Unit unit;
 
-  /** Horizontal coordinate of the tile */
-  protected int x;
+    /** Horizontal coordinate of the tile */
+    protected int x;
 
-  /** Vertical coordinate of the tile */
-  protected int y;
+    /** Vertical coordinate of the tile */
+    protected int y;
 
-  /**
-   * Creates a new tile.
-   */
-  public Tile(int x, int y) {
-    this.unit = null;
-    this.x = x;
-    this.y = y;
-  }
-
-  /**
-   * Return the horizontal coordinate of the tile
-   * 
-   * @return Horizontal coordinate
-   */
-  public int getX() {
-    return this.x;
-  }
-
-  /**
-   * Return the vertical coordinate of the tile
-   * 
-   * @return Vertical coordinate
-   */
-  public int getY() {
-    return this.y;
-  }
-
-  /**
-   * This allows to know if a tile is busy or not.
-   * 
-   * @return true if the tile is busy, otherwise false.
-   */
-  public boolean isBusy() {
-    return (this.unit != null);
-  }
-
-  /**
-   * Gives the ressource given by the tile.
-   */
-  public abstract Resource getResource() throws IllegalGameActionException;
-
-  /**
-   * Gives the unit posted on the tile.
-   * 
-   * @return The unit posted on the tile.
-   */
-  public Unit getUnit() {
-    return this.unit;
-  }
-
-  /**
-   * This allows to change the unit post on the tile.
-   * 
-   * @param u the unit that we want to post on the tile
-   * @throws IllegalArgumentException when null is given as an argument.
-   * @throws IllegalGameActionException if the set is forbidden (eg. on OceanTile)
-   */
-  public void setUnit(Unit u) throws IllegalArgumentException, IllegalGameActionException {
-    if (u == null) {
-      throw new IllegalArgumentException("Tried to set null as unit");
+    /**
+     * Creates a new tile.
+     */
+    public Tile(int x, int y) {
+        this.unit = null;
+        this.x = x;
+        this.y = y;
     }
-    this.unit = u;
-  }
 
-  /**
-   * Make sure that there is no unit placed in this tile after the call of this
-   * method.
-   */
-  public void unsetUnit() {
-    this.unit = null;
-  }
+    /**
+     * Return the horizontal coordinate of the tile
+     *
+     * @return Horizontal coordinate
+     */
+    public int getX() {
+        return this.x;
+    }
 
-  /**
-   * return the bonus gold a player may receive from each of his unit placed on
-   * this type of tile when among the selectable actions he chooses to do nothing.
-   * 
-   * <code>GOLD_WHEN_DOING_NOTHING</code> has to be initialized before running the
-   * game (Game#play) to avoid undefined behavior.
-   * 
-   * @return The bonus gold a player may receive from each of his unit placed on
-   *         this type of tile when among the selectable actions he chooses to do
-   *         nothing.
-   */
-  public abstract int getGoldWhenDoingNothing();
+    /**
+     * Return the vertical coordinate of the tile
+     *
+     * @return Vertical coordinate
+     */
+    public int getY() {
+        return this.y;
+    }
 
-  /**
-   * return The amounts of additional (bonus) power that an unit placed on this
-   * type of tile may receives.
-   * 
-   * @return The amounts of additional (bonus) power that an unit placed on this
-   *         type of tile may receives.
-   */
-  public abstract int getAdditionnalPower();
+    /**
+     * This allows to know if a tile is busy or not.
+     *
+     * @return true if the tile is busy, otherwise false.
+     */
+    public boolean isBusy() {
+        return (this.unit != null);
+    }
 
-  /**
-   * return the bonus a player may receive from each of his unit placed on this
-   * type of tile when counting the total points at the end of the game.
-   * 
-   * @return The bonus a player may receive from each of his unit placed on this
-   *         type of tile when counting the total points at the end of the game.
-   */
-  public abstract int getAdditionnalPoints();
+    /**
+     * Gives the ressource given by the tile.
+     */
+    public abstract Resource getResource() throws IllegalGameActionException;
 
-  /**
-   * Return the maximum army size allowed on a tile
-   * 
-   * @return Maximum army size
-   */
-  public abstract int getMaxArmySize();
+    /**
+     * Gives the unit posted on the tile.
+     *
+     * @return The unit posted on the tile.
+     */
+    public Unit getUnit() {
+        return this.unit;
+    }
 
-  /**
-   * return the bonus to be added to the initial cost of an unit placed on this
-   * tile.
-   * 
-   * @return the bonus to be added to the initial cost of an unit placed on this
-   *         tile.
-   */
-  public abstract int getCostAdd();
+    /**
+     * This allows to change the unit post on the tile.
+     *
+     * @param u the unit that we want to post on the tile
+     * @throws IllegalArgumentException when null is given as an argument.
+     * @throws IllegalGameActionException if the set is forbidden (eg. on OceanTile)
+     */
+    public void setUnit(Unit u) throws IllegalArgumentException, IllegalGameActionException {
+        if (u == null) {
+            throw new IllegalArgumentException("Tried to set null as unit");
+        }
+        this.unit = u;
+    }
 
-  /**
-   * return the factor with which to multiply the initial cost of an unit placed
-   * on this tile.
-   * 
-   * @return the factor with which to multiply the initial cost of an unit placed
-   *         on this tile.
-   */
-  public abstract int getCostFactor();
+    /**
+     * Make sure that there is no unit placed in this tile after the call of this
+     * method.
+     */
+    public void unsetUnit() {
+        this.unit = null;
+    }
+
+    /**
+     * return the bonus gold a player may receive from each of his unit placed on
+     * this type of tile when among the selectable actions he chooses to do nothing.
+     *
+     * <code>GOLD_WHEN_DOING_NOTHING</code> has to be initialized before running the
+     * game (Game#play) to avoid undefined behavior.
+     *
+     * @return The bonus gold a player may receive from each of his unit placed on
+     *         this type of tile when among the selectable actions he chooses to do
+     *         nothing.
+     */
+    public abstract int getGoldWhenDoingNothing();
+
+    /**
+     * return The amounts of additional (bonus) power that an unit placed on this
+     * type of tile may receives.
+     *
+     * @return The amounts of additional (bonus) power that an unit placed on this
+     *         type of tile may receives.
+     */
+    public abstract int getAdditionnalPower();
+
+    /**
+     * return the bonus a player may receive from each of his unit placed on this
+     * type of tile when counting the total points at the end of the game.
+     *
+     * @return The bonus a player may receive from each of his unit placed on this
+     *         type of tile when counting the total points at the end of the game.
+     */
+    public abstract int getAdditionnalPoints();
+
+    /**
+     * Return the maximum army size allowed on a tile
+     *
+     * @return Maximum army size
+     */
+    public abstract int getMaxArmySize();
+
+    /**
+     * return the bonus to be added to the initial cost of an unit placed on this
+     * tile.
+     *
+     * @return the bonus to be added to the initial cost of an unit placed on this
+     *         tile.
+     */
+    public abstract int getCostAdd();
+
+    /**
+     * return the factor with which to multiply the initial cost of an unit placed
+     * on this tile.
+     *
+     * @return the factor with which to multiply the initial cost of an unit placed
+     *         on this tile.
+     */
+    public abstract int getCostFactor();
 
     public void print() {
         if (this.unit != null) {
--- test/game/tile/TileMockTest.java
+++ test/game/tile/TileMockTest.java
@@ -5,66 +5,68 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import game.exception.IllegalGameActionException;
 import game.tile.TileMock;
 import game.unit.Unit;
 import game.unit.Worker;
 
 public class TileMockTest {
 
-	private TileMock tile;
-	private Unit unit;
-	
-	@Before
-	public void setUp() throws Exception {
-		tile = new TileMock();
-		unit = new Worker(null, null);
-	}
+    private TileMock tile;
+    private Unit unit;
 
-	@Test
-	public void testGetResource() {
-		assertNull(tile.getResource()); // It's null since this is just a mock tile.
-	}
+    @Before
+    public void setUp() throws Exception {
+        tile = new TileMock(5, 12);
+        unit = new Worker(null, null);
+    }
 
-	@Test
-	public void testConstructor() {
-		assertFalse(tile.isBusy());
-		assertNull(tile.getUnit());
-	}
+    @Test
+    public void canGetCoordinates() {
+        assertEquals(5, this.tile.getX());
+        assertEquals(12, this.tile.getY());
+    }
 
-	@Test
-	public void testIsBusy() {
-		assertFalse(tile.isBusy());
-		
-		tile.setUnit(unit);
-		assertTrue(tile.isBusy());
-	}
+    @Test
+    public void testConstructor() {
+        assertFalse(tile.isBusy());
+        assertNull(tile.getUnit());
+    }
 
-	@Test
-	public void testGetUnit() {
-		assertNull(tile.getUnit());
-		
-		
-		tile.setUnit(unit);
-		assertSame(unit, tile.getUnit());
-	}
+    @Test
+    public void testIsBusy() throws IllegalGameActionException {
+        assertFalse(tile.isBusy());
 
-	@Test
-	public void testSetUnit() {
-		tile.setUnit(unit);
-		assertSame(unit, tile.getUnit());
-		
-		tile.setUnit(null);
-		assertSame(unit, tile.getUnit());
-	}
-	
-	@Test
-	public void testUnsetUnit() {
-		assertFalse(tile.isBusy());
-		tile.unsetUnit();
-		
-		tile.setUnit(unit);
-		tile.unsetUnit();
-		assertFalse(tile.isBusy());
-	}
+        tile.setUnit(unit);
+        assertTrue(tile.isBusy());
+    }
 
+    @Test
+    public void testGetUnit() throws IllegalGameActionException {
+        assertNull(tile.getUnit());
+
+        tile.setUnit(unit);
+        assertSame(unit, tile.getUnit());
+    }
+
+    @Test
+    public void testSetUnit() throws IllegalGameActionException {
+        tile.setUnit(unit);
+        assertSame(unit, tile.getUnit());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void cannotSetUnitWithNullArgument() throws IllegalGameActionException {
+        tile.setUnit(null);
+    }
+
+    @Test
+    public void testUnsetUnit() throws IllegalGameActionException {
+        assertFalse(tile.isBusy());
+        tile.unsetUnit();
+
+        tile.setUnit(unit);
+        tile.unsetUnit();
+        assertFalse(tile.isBusy());
+    }
 }
