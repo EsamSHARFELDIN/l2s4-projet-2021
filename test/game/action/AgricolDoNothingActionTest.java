82ccfa685fda53a6032e645646bb33d303261b24
zo <zotahina.ratefiarivony.etu@univ-lille.fr>
2021-05-15 18:45:02 +0200

    test: AgricolDoNothingActionTest, fix canExecuteAction()

--- test/game/action/AgricolDoNothingActionTest.java
+++ test/game/action/AgricolDoNothingActionTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import game.Board;
 import game.action.Action;
 import game.action.AgricolDoNothingAction;
+import game.exception.GameException;
 import game.player.AgricolPlayer;
 import game.player.Player;
 import game.tile.DesertTile;
@@ -23,23 +24,25 @@ public class AgricolDoNothingActionTest {
   }
 
   @Test
-  public void canExecuteAction() {
+  public void canExecuteAction() throws GameException {
     /* Construire joueur */
     Player p = new AgricolPlayer("André");
     Board b = new Board(15, 10);
+    
     /* Construire tuiles, workers, les placer sur les tuiles */ 
-    Tile t1 = new PlainTile();
-    Tile t2 = new PlainTile();
-    Tile t3 = new PlainTile();
-    Tile t4 = new ForestTile();
-    Tile t5 = new ForestTile();
-    Tile t6 = new DesertTile();
+    Tile t1 = new PlainTile(0, 0);
+    Tile t2 = new PlainTile(1, 1);
+    Tile t3 = new PlainTile(2, 2);
+    Tile t4 = new ForestTile(3, 3);
+    Tile t5 = new ForestTile(4, 4);
+    Tile t6 = new DesertTile(5, 5);
     Unit u1 = new Worker(t1, p);
     Unit u2 = new Worker(t2, p);
     Unit u3 = new Worker(t3, p);
     Unit u4 = new Worker(t4, p);
     Unit u5 = new Worker(t5, p);
     Unit u6 = new Worker(t6, p);
+    
     /* Ajouter les unités */
     p.addUnit(u1);
     p.addUnit(u2);
@@ -47,11 +50,14 @@ public class AgricolDoNothingActionTest {
     p.addUnit(u4);
     p.addUnit(u5);
     p.addUnit(u6);
+    
     /* Mémoriser sa quantité d'or initiale */
     int initialGold = p.getGold();
+    
     /* Executer execute */
     Action a = new AgricolDoNothingAction();
-    a.execute(p, b);
+    a.execute(b, p);
+    
     /* Calculer la quantité d'or supplémentaire que le joueur doit avoir gagné = 7 */
 
     /* assertEquals quantité initiale + surplus = nouvelle quantité */
