3296d4149cc817a80aaf3cde4fc7f67e31a2e562
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-05-16 21:30:51 +0200

    fix: DeployActionTest
     * Rewrote canExecuteAction to use ExampleBoard

--- tests/game/action/DeployActionTest.java
+++ tests/game/action/DeployActionTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 
 import game.action.Action;
 import game.action.DeployAction;
-import game.board.RandomBoard;
+import game.board.Board;
+import game.board.ExampleBoard;
 import game.exception.GameException;
 import game.player.AgricolPlayer;
 import game.player.Player;
@@ -14,22 +15,22 @@ import game.unit.Unit;
 import game.unit.Worker;
 
 public class DeployActionTest {
-    
+
     @Test
     public void canConstructTest() {
-	    Unit u1 = new Worker();
-	    Action a = new DeployAction(12, 10, u1);
+        Unit u1 = new Worker();
+        Action a = new DeployAction(12, 10, u1);
     }
-        
+
     @Test
-    public void canExecuteAction() throws GameException{
-	    RandomBoard b1 = new RandomBoard(15, 10);
-	    Player p1 = new AgricolPlayer("Esam");
-	    
-	    /* Executer execute */
-	    Unit u1 = new Worker();
-	    Action a = new DeployAction(12, 10, u1);
-	    a.execute(b1, p1);
-	    assertSame(b1.tileAt(12, 10).getUnit(), u1);
+    public void canExecuteAction() throws GameException {
+        Board b1 = new ExampleBoard();
+        Player p1 = new AgricolPlayer("Esam");
+
+        /* Executer execute */
+        Unit u1 = new Worker();
+        Action a = new DeployAction(2, 3, u1);
+        a.execute(b1, p1);
+        assertSame(b1.tileAt(2, 3).getUnit(), u1);
     }
 }
