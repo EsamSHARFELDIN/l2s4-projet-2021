4504e160310c5bb29d28b0d2b68c6a3acfdf7d03
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-05-16 21:23:41 +0200

    fix: ResourceTest
     * Removed test for setConversionValue with null

--- src/game/Resource.java
+++ src/game/Resource.java
@@ -1,58 +1,58 @@
 package game;
 
 /**
- * 
+ *
  * representing the different types of resources, that the characters of the game can collect on the different tiles.
  *
  */
 public enum Resource {
-  
-	Stone, Sand, Wheat, Wood;
-	
-	private static int STONE_CONVERSION_VALUE;
-	private static int SAND_CONVERSION_VALUE;
-	private static int WHEAT_CONVERSION_VALUE;
-	private static int WOOD_CONVERSION_VALUE;
 
-  /*
-  * set the conversion value of the resource r to n.
-  * @param r the resource that we want to modify the conversion value.
-  * @param i the new conversion value.
-  */
-	public static void setConversionValue(Resource r, int i) {
-    switch (r) {
-      case Stone:
-      STONE_CONVERSION_VALUE = i;
-      break;
-      case Wheat:
-      WHEAT_CONVERSION_VALUE = i;
-      break;
-      case Wood:
-      WOOD_CONVERSION_VALUE = i;
-      break;
-      case Sand : 
-      SAND_CONVERSION_VALUE = i;
-      break;
-    }
-	}
+    Stone, Sand, Wheat, Wood;
 
-  /*
-  * gives the conversion value of the resource.
-  * @param r the resource we want to get the conversion value.
-  * @return the conversion value of the resource.
-  */
-	public static int getConversionValue(Resource r) {
-		switch (r) {
-      case Stone:
-      return STONE_CONVERSION_VALUE;
-      case Wheat:
-      return WHEAT_CONVERSION_VALUE;
-      case Wood:
-      return WOOD_CONVERSION_VALUE;
-      case Sand : 
-      return SAND_CONVERSION_VALUE;
-      default:
-      return -1; /* ne devrait pas arriver */
+    private static int STONE_CONVERSION_VALUE;
+    private static int SAND_CONVERSION_VALUE;
+    private static int WHEAT_CONVERSION_VALUE;
+    private static int WOOD_CONVERSION_VALUE;
+
+    /*
+     * set the conversion value of the resource r to n.
+     * @param r the resource that we want to modify the conversion value.
+     * @param i the new conversion value.
+     */
+    public static void setConversionValue(Resource r, int i) {
+        switch (r) {
+        case Stone:
+            STONE_CONVERSION_VALUE = i;
+            break;
+        case Wheat:
+            WHEAT_CONVERSION_VALUE = i;
+            break;
+        case Wood:
+            WOOD_CONVERSION_VALUE = i;
+            break;
+        case Sand :
+            SAND_CONVERSION_VALUE = i;
+            break;
+        }
     }
-	}
+
+    /*
+     * gives the conversion value of the resource.
+     * @param r the resource we want to get the conversion value.
+     * @return the conversion value of the resource.
+     */
+    public static int getConversionValue(Resource r) {
+        switch (r) {
+        case Stone:
+            return STONE_CONVERSION_VALUE;
+        case Wheat:
+            return WHEAT_CONVERSION_VALUE;
+        case Wood:
+            return WOOD_CONVERSION_VALUE;
+        case Sand :
+            return SAND_CONVERSION_VALUE;
+        default:
+            return -1; /* ne devrait pas arriver */
+        }
+    }
 }
--- tests/game/ResourceTest.java
+++ tests/game/ResourceTest.java
@@ -7,46 +7,38 @@ import org.junit.Test;
 
 public class ResourceTest {
 
-	@Before
-	public void setUp() throws Exception {
-		Resource.setConversionValue(Resource.Stone, 0);
-		Resource.setConversionValue(Resource.Sand, 1);
-		Resource.setConversionValue(Resource.Wheat, 2);
-		Resource.setConversionValue(Resource.Wood, 3);
-	}
+    @Before
+    public void setUp() throws Exception {
+        Resource.setConversionValue(Resource.Stone, 0);
+        Resource.setConversionValue(Resource.Sand, 1);
+        Resource.setConversionValue(Resource.Wheat, 2);
+        Resource.setConversionValue(Resource.Wood, 3);
+    }
 
-	@Test
-	public void testSetConversionValue() {
-		Resource.setConversionValue(Resource.Stone, 0);
-		Resource.setConversionValue(Resource.Sand, 10);
-		Resource.setConversionValue(Resource.Wheat, 20);
-		Resource.setConversionValue(Resource.Wood, 30);
-		
-		assertSame(0, Resource.getConversionValue(Resource.Stone));
-		assertSame(10, Resource.getConversionValue(Resource.Sand));
-		assertSame(20, Resource.getConversionValue(Resource.Wheat));
-		assertSame(30, Resource.getConversionValue(Resource.Wood));
-		
-		Resource.setConversionValue(null, -1); //test when null is given as argument for r
-		
-		//nothing should have been changed
-		assertSame(0, Resource.getConversionValue(Resource.Stone));
-		assertSame(10, Resource.getConversionValue(Resource.Sand));
-		assertSame(20, Resource.getConversionValue(Resource.Wheat));
-		assertSame(30, Resource.getConversionValue(Resource.Wood));
-	}
+    @Test
+    public void testSetConversionValue() {
+        Resource.setConversionValue(Resource.Stone, 0);
+        Resource.setConversionValue(Resource.Sand, 10);
+        Resource.setConversionValue(Resource.Wheat, 20);
+        Resource.setConversionValue(Resource.Wood, 30);
 
-	@Test
-	public void getConversionValueNormally() {
-		assertSame(0, Resource.getConversionValue(Resource.Stone));
-		assertSame(1, Resource.getConversionValue(Resource.Sand));
-		assertSame(2, Resource.getConversionValue(Resource.Wheat));
-		assertSame(3, Resource.getConversionValue(Resource.Wood));
-	}
-	
-	@Test(expected=NullPointerException.class)
-	public void getConversionValueOfNull() {
-		Resource.getConversionValue(null);
-	}
+        assertSame(0, Resource.getConversionValue(Resource.Stone));
+        assertSame(10, Resource.getConversionValue(Resource.Sand));
+        assertSame(20, Resource.getConversionValue(Resource.Wheat));
+        assertSame(30, Resource.getConversionValue(Resource.Wood));
+    }
+
+    @Test
+    public void getConversionValueNormally() {
+        assertSame(0, Resource.getConversionValue(Resource.Stone));
+        assertSame(1, Resource.getConversionValue(Resource.Sand));
+        assertSame(2, Resource.getConversionValue(Resource.Wheat));
+        assertSame(3, Resource.getConversionValue(Resource.Wood));
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void getConversionValueOfNull() {
+        Resource.getConversionValue(null);
+    }
 
 }
