5347fb2d32275a9a557c3e0ea4a4fb1d993bf7a9
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-05-17 18:33:59 +0200

    fix: AgricolAction tests need AgricolGame context to be set
     * Initialized "global" variables in use in test classes AgricolDoNothingActionTest
       and AgricolExchangeActionTest
     * Fixed expected initial gold of AgricolPlayer to be 15

--- tests/game/action/AgricolDoNothingActionTest.java
+++ tests/game/action/AgricolDoNothingActionTest.java
@@ -2,65 +2,100 @@ package game.action;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import game.Resource;
 import game.action.Action;
 import game.action.AgricolDoNothingAction;
 import game.board.RandomBoard;
 import game.exception.GameException;
 import game.player.AgricolPlayer;
 import game.player.Player;
-import game.tile.DesertTile;
-import game.tile.ForestTile;
-import game.tile.PlainTile;
-import game.tile.Tile;
+import game.tile.*;
 import game.unit.Unit;
 import game.unit.Worker;
 
 public class AgricolDoNothingActionTest {
-  @Test
-  public void canConstructAction() {
-    Action a = new AgricolDoNothingAction();
-  }
+    @BeforeClass
+    public static void setAgricolGameContext() {
+        Resource.setConversionValue(Resource.Stone, 8);
+        Resource.setConversionValue(Resource.Sand, 5);
+        Resource.setConversionValue(Resource.Wood, 2);
+        Resource.setConversionValue(Resource.Wheat, 2);
 
-  @Test
-  public void canExecuteAction() throws GameException {
-    /* Construire joueur */
-    Player p = new AgricolPlayer("André");
-    RandomBoard b = new RandomBoard(15, 10);
-    
-    /* Construire tuiles, workers, les placer sur les tuiles */ 
-    Tile t1 = new PlainTile(0, 0);
-    Tile t2 = new PlainTile(1, 1);
-    Tile t3 = new PlainTile(2, 2);
-    Tile t4 = new ForestTile(3, 3);
-    Tile t5 = new ForestTile(4, 4);
-    Tile t6 = new DesertTile(5, 5);
-    Unit u1 = new Worker(t1, p);
-    Unit u2 = new Worker(t2, p);
-    Unit u3 = new Worker(t3, p);
-    Unit u4 = new Worker(t4, p);
-    Unit u5 = new Worker(t5, p);
-    Unit u6 = new Worker(t6, p);
-    
-    /* Ajouter les unités */
-    p.addUnit(u1);
-    p.addUnit(u2);
-    p.addUnit(u3);
-    p.addUnit(u4);
-    p.addUnit(u5);
-    p.addUnit(u6);
-    
-    /* Mémoriser sa quantité d'or initiale */
-    int initialGold = p.getGold();
-    
-    /* Executer execute */
-    Action a = new AgricolDoNothingAction();
-    a.execute(b, p);
-    
-    /* Calculer la quantité d'or supplémentaire que le joueur doit avoir gagné = 7 */
+        PlainTile.MAX_ARMY_SIZE = 1;
+        PlainTile.ADDITIONAL_POWER = 0;
+        PlainTile.ADDITIONAL_POINTS = 0;
+        PlainTile.GOLD_WHEN_DOING_NOTHING = 1;
+        PlainTile.COST_ADD = 1;
+        PlainTile.COST_FACTOR = 1;
 
-    /* assertEquals quantité initiale + surplus = nouvelle quantité */
-    assertEquals(initialGold + 7, p.getGold());
-  }
+        MountainTile.MAX_ARMY_SIZE = 1;
+        MountainTile.ADDITIONAL_POWER = 0;
+        MountainTile.ADDITIONAL_POINTS = 0;
+        MountainTile.GOLD_WHEN_DOING_NOTHING = 0;
+        MountainTile.COST_ADD = 5;
+        MountainTile.COST_FACTOR = 1;
+
+        ForestTile.MAX_ARMY_SIZE = 1;
+        ForestTile.ADDITIONAL_POWER = 0;
+        ForestTile.ADDITIONAL_POINTS = 0;
+        ForestTile.GOLD_WHEN_DOING_NOTHING = 1;
+        ForestTile.COST_ADD = 1;
+        ForestTile.COST_FACTOR = 1;
+
+        DesertTile.MAX_ARMY_SIZE = 1;
+        DesertTile.ADDITIONAL_POWER = 0;
+        DesertTile.ADDITIONAL_POINTS = 0;
+        DesertTile.GOLD_WHEN_DOING_NOTHING = 2;
+        DesertTile.COST_ADD = 3;
+        DesertTile.COST_FACTOR = 1;
+    }
+
+    @Test
+    public void canConstructAction() {
+        Action a = new AgricolDoNothingAction();
+    }
+
+    @Test
+    public void canExecuteAction() throws GameException {
+        /* Construire joueur */
+        Player p = new AgricolPlayer("André");
+        RandomBoard b = new RandomBoard(15, 10);
+
+        /* Construire tuiles, workers, les placer sur les tuiles */
+        Tile t1 = new PlainTile(0, 0);
+        Tile t2 = new PlainTile(1, 1);
+        Tile t3 = new PlainTile(2, 2);
+        Tile t4 = new ForestTile(3, 3);
+        Tile t5 = new ForestTile(4, 4);
+        Tile t6 = new DesertTile(5, 5);
+        Unit u1 = new Worker(t1, p);
+        Unit u2 = new Worker(t2, p);
+        Unit u3 = new Worker(t3, p);
+        Unit u4 = new Worker(t4, p);
+        Unit u5 = new Worker(t5, p);
+        Unit u6 = new Worker(t6, p);
+
+        /* Ajouter les unités */
+        p.addUnit(u1);
+        p.addUnit(u2);
+        p.addUnit(u3);
+        p.addUnit(u4);
+        p.addUnit(u5);
+        p.addUnit(u6);
+
+        /* Mémoriser sa quantité d'or initiale */
+        int initialGold = p.getGold();
+
+        /* Executer execute */
+        Action a = new AgricolDoNothingAction();
+        a.execute(b, p);
+
+        /* Calculer la quantité d'or supplémentaire que le joueur doit avoir gagné = 7 */
+
+        /* assertEquals quantité initiale + surplus = nouvelle quantité */
+        assertEquals(initialGold + 7, p.getGold());
+    }
 }
--- tests/game/action/AgricolExchangeActionTest.java
+++ tests/game/action/AgricolExchangeActionTest.java
@@ -2,6 +2,7 @@ package game.action;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import game.Resource;
@@ -11,8 +12,44 @@ import game.board.RandomBoard;
 import game.exception.GameException;
 import game.player.AgricolPlayer;
 import game.player.Player;
+import game.tile.*;
 
 public class AgricolExchangeActionTest {
+    @BeforeClass
+    public static void setAgricolGameContext() {
+        Resource.setConversionValue(Resource.Stone, 8);
+        Resource.setConversionValue(Resource.Sand, 5);
+        Resource.setConversionValue(Resource.Wood, 2);
+        Resource.setConversionValue(Resource.Wheat, 2);
+
+        PlainTile.MAX_ARMY_SIZE = 1;
+        PlainTile.ADDITIONAL_POWER = 0;
+        PlainTile.ADDITIONAL_POINTS = 0;
+        PlainTile.GOLD_WHEN_DOING_NOTHING = 1;
+        PlainTile.COST_ADD = 1;
+        PlainTile.COST_FACTOR = 1;
+
+        MountainTile.MAX_ARMY_SIZE = 1;
+        MountainTile.ADDITIONAL_POWER = 0;
+        MountainTile.ADDITIONAL_POINTS = 0;
+        MountainTile.GOLD_WHEN_DOING_NOTHING = 0;
+        MountainTile.COST_ADD = 5;
+        MountainTile.COST_FACTOR = 1;
+
+        ForestTile.MAX_ARMY_SIZE = 1;
+        ForestTile.ADDITIONAL_POWER = 0;
+        ForestTile.ADDITIONAL_POINTS = 0;
+        ForestTile.GOLD_WHEN_DOING_NOTHING = 1;
+        ForestTile.COST_ADD = 1;
+        ForestTile.COST_FACTOR = 1;
+
+        DesertTile.MAX_ARMY_SIZE = 1;
+        DesertTile.ADDITIONAL_POWER = 0;
+        DesertTile.ADDITIONAL_POINTS = 0;
+        DesertTile.GOLD_WHEN_DOING_NOTHING = 2;
+        DesertTile.COST_ADD = 3;
+        DesertTile.COST_FACTOR = 1;
+    }
     
     @Test 
     public void canConstructorTest() {
@@ -27,11 +64,11 @@ public class AgricolExchangeActionTest {
       Player p1 = new AgricolPlayer("Kilian");
       
       a.execute(b1, p1);
-      assertEquals(p1.getResource(Resource.Stone), 0);
-      assertEquals(p1.getResource(Resource.Sand), 0);
-      assertEquals(p1.getResource(Resource.Wheat), 0);
-      assertEquals(p1.getResource(Resource.Wood), 0);
-      assertEquals(p1.getGold(), 0);
+      assertEquals(0, p1.getResource(Resource.Stone));
+      assertEquals(0, p1.getResource(Resource.Sand));
+      assertEquals(0, p1.getResource(Resource.Wheat));
+      assertEquals(0, p1.getResource(Resource.Wood));
+      assertEquals(15, p1.getGold());
     }
 
     @Test
@@ -51,11 +88,11 @@ public class AgricolExchangeActionTest {
       p2.incrementResource(woodStock, Resource.Wood);
 
       a.execute(b1, p2);
-      assertEquals(p2.getResource(Resource.Stone), 0);
-      assertEquals(p2.getResource(Resource.Sand), 0);
-      assertEquals(p2.getResource(Resource.Wheat), 0);
-      assertEquals(p2.getResource(Resource.Wood), 0);
-      assertEquals(p2.getGold(), 38);
+      assertEquals(0, p2.getResource(Resource.Stone));
+      assertEquals(0, p2.getResource(Resource.Sand));
+      assertEquals(0, p2.getResource(Resource.Wheat));
+      assertEquals(0, p2.getResource(Resource.Wood));
+      assertEquals(53, p2.getGold());
     }
 }
 
