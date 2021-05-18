65468bdf08b21532de3c79a0ed08df0732498984
zo <zotahina.ratefiarivony.etu@univ-lille.fr>
2021-05-18 08:05:03 +0200

    fix: PlayerMock, the parameter type of chooseAction() become Board
    * add a little doc

--- src/game/player/PlayerMock.java
+++ src/game/player/PlayerMock.java
@@ -2,16 +2,22 @@ package game.player;
 
 import game.action.Action;
 import game.action.DoNothingAction;
+import game.board.Board;
 import game.board.RandomBoard;
 import game.unit.Unit;
 
+/**
+ * This class is only used for test purpose of the abstract
+ * class Player.
+ *
+ */
 public class PlayerMock extends Player {
     public PlayerMock(String name, int rock, int sand, int wheat,
                       int wood, int gold) {
         super(name, rock, sand, wheat, wood, gold);
     }
 
-    public Action chooseAction(RandomBoard board) {
+    public Action chooseAction(Board board) {
         return new DoNothingAction();
     }
 
