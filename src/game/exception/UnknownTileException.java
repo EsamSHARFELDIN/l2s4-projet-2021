eea90b7c98989dd70f8926d38efb7394b22a5ca6
zo <zotahina.ratefiarivony.etu@univ-lille.fr>
2021-05-20 07:02:11 +0200

    doc: improve general doc for UnknownTileException

--- src/game/exception/UnknownTileException.java
+++ src/game/exception/UnknownTileException.java
@@ -1,7 +1,9 @@
 package game.exception;
 
 /**
- * An exception class to indicate that an invalid tile was accessed
+ * An exception class to indicate that an invalid tile was accessed.
+ * For example, in a board when trying to access a tile which does not 
+ * exist.
  */
 public class UnknownTileException extends GameException {
     public UnknownTileException() {
